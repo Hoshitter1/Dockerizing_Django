17c927e6f194597ca7240a5868a3e0533c02dcc9
Hoshitter1 <hoshi.to.318@gmail.com>
2020-01-19 14:57:51 +0900

    Updated some files for #11 #5

--- django/base_app/views.py
+++ django/base_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# import django_filters
+from rest_framework import viewsets, filters
+from .models import User, Entry
+from .serializer import UserSerializer, EntrySerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class EntryViewSet(viewsets.ModelViewSet):
+    queryset = Entry.objects.all()
+    serializer_class = EntrySerializer
--- django/my_docker_project/urls.py
+++ django/my_docker_project/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+
+from base_app.urls import router as base
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url(r'^api/', include(base.urls)),
 ]
